52ca05928b7f11451455de8e7abbc21cfd27b113
cassiocmps <cassio.szcampos@gmail.com>
2024-03-02 16:01:27 -0300

    [NEW] Open Closed Principle

---
+++ OpenClosedPrinciple/ValidPayrollCalculator.cs
@@ -0,0 +1,43 @@
+﻿namespace ValidPayrollCalculator;
+
+internal class ValidPayrollCalculator
+{
+    protected double Balance;
+
+    // By using inversion of control, now ValidPayrollCalculator.CalculatePayment can work with any class that implements IRemunerated
+    // without needing to modify its implementation
+
+    public void CalculatePayment(IEmploymentContract employee)
+    {
+        Balance = employee.CalculateCompensation();
+    }
+}
+
+internal interface IEmploymentContract
+{
+    public double CalculateCompensation();
+}
+
+internal class Employee : IEmploymentContract
+{
+    public double CalculateCompensation()
+    {
+        return 0;
+    }
+}
+
+internal class Intern : IEmploymentContract
+{
+    public double CalculateCompensation()
+    {
+        return 0;
+    }
+}
+
+internal class Contractor : IEmploymentContract
+{
+    public double CalculateCompensation()
+    {
+        return 0;
+    }
+}
