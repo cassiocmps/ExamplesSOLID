25c05a3188d975d41b8b94219c1de0a670339cbe
cassiocmps <cassio.szcampos@gmail.com>
2024-03-02 15:07:07 -0300

    [NEW] start

---
+++ Common/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common;
+
+public class Employee
+{
+    public string Name { get; set; }
+    public int Id { get; set; }
+    public decimal Salary { get; set; }
+}
