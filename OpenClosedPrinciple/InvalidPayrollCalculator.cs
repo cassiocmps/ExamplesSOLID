ad8ab501ec5f9a056720f2925c87732303d30ca6
cassiocmps <cassio.szcampos@gmail.com>
2024-03-02 16:01:27 -0300

    [NEW] Open Closed Principle

---
+++ OpenClosedPrinciple/InvalidPayrollCalculator.cs
@@ -0,0 +1,41 @@
+﻿namespace InvalidPayrollCalculator;
+
+public class InvalidPayrollCalculator
+{
+    protected double Balance;
+
+    // Imagine we have this calculator where different rules applies to different employment formats
+    // and we now want to add a Contractor model.
+    // It is possible to just add another 'if' statement in the method,
+    // but this approach could introduce bugs in a process already stablished 
+
+    public void CalculatePayment(object employee)
+    {
+        if (employee is Employee)
+        {
+            Employee contract = (Employee)employee;
+            Balance = contract.GetSalary();
+        }
+        else if (employee is Intern)
+        {
+            Intern internship = (Intern)employee;
+            Balance = internship.GetAllowance();
+        }
+    }
+}
+
+internal class Employee
+{
+    public double GetSalary()
+    {
+        return 0;
+    }
+}
+
+internal class Intern
+{
+    public double GetAllowance()
+    {
+        return 0;
+    }
+}
---
+++ OpenClosedPrinciple/ValidPayrollCalculator.cs
@@ -0,0 +1,43 @@
+﻿namespace ValidPayrollCalculator;
+
+internal class ValidPayrollCalculator
+{
+    protected double Balance;
+
+    // By using inversion of control, now ValidPayrollCalculator.CalculatePayment can work with any class that implements IRemunerated
+    // without needing to modify its implementation
+
+    public void CalculatePayment(IRemunerated employee)
+    {
+        Balance = employee.CalculatePayment();
+    }
+}
+
+internal interface IRemunerated
+{
+    public double CalculatePayment();
+}
+
+internal class Employee : IRemunerated
+{
+    public double CalculatePayment()
+    {
+        return 0;
+    }
+}
+
+internal class Intern : IRemunerated
+{
+    public double CalculatePayment()
+    {
+        return 0;
+    }
+}
+
+internal class Contractor : IRemunerated
+{
+    public double CalculatePayment()
+    {
+        return 0;
+    }
+}
